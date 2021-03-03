f6a15b316244e44f94b0f4b847d64dd273a1faac
Beekasha <brian.j.kleiner@gmail.com>
2021-03-03 13:33:20 -0500

    add environment variables to wav conversion

--- convert_to_wav.py
+++ convert_to_wav.py
@@ -1,20 +1,26 @@
+from dotenv import load_dotenv
+load_dotenv()
+
 import os
 from pydub import AudioSegment
 
-# path to audio files directory
-path = "../htown/samples"
 
-#Change working directory
-os.chdir(path)
 
-audio_files = os.listdir()
+def mp3_to_wav(pathname):
+    #Change working directory
+    os.chdir(pathname)
+    audio_files = os.listdir()
 
-# You dont need the number of files in the folder, just iterate over them directly using:
-for file in audio_files:
-    #spliting the file into the name and the extension
-    name, ext = os.path.splitext(file)
-    if ext == ".mp3":
-       mp3_sound = AudioSegment.from_mp3(file)
-       #rename them using the old name + ".wav"
-       mp3_sound.export("{0}.wav".format(name), format="wav")
+    print(f'We have {len(audio_files)} files to convert')
+    for file in audio_files:
+        #spliting the file into the name and the extension
+        name, ext = os.path.splitext(file)
+        
+        if ext == ".mp3":
+            mp3_sound = AudioSegment.from_mp3(file)
+            print(f'printing {name}')
+            #rename them using the old name + ".wav"
+            mp3_sound.export("{0}.wav".format(name), format="wav")
 
+# mp3_to_wav(path)
+mp3_to_wav(os.environ.get("directory-of-mp3-test"))

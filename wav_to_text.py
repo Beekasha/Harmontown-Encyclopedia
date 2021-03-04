18e78c99a04b7697156e025cbfcc26985f017117
Beekasha <brian.j.kleiner@gmail.com>
2021-03-04 16:43:41 -0500

    wav_to_text recognizing all audio files

--- wav_to_text.py
+++ wav_to_text.py
@@ -1,4 +1,8 @@
 import speech_recognition as sr
+
+from dotenv import load_dotenv
+load_dotenv()
+
 import os
 from pydub import AudioSegment
 
@@ -16,7 +20,7 @@ from pydub import AudioSegment
 
 
 
-def transcribe(audio_path):
+def transcribe_wav(audio_path):
     r = sr.Recognizer()
 
     harvard = sr.AudioFile(audio_path)
@@ -26,7 +30,39 @@ def transcribe(audio_path):
         trans = r.recognize_google(audio)
         print(trans)
 
-transcribe('./testDump/compressedHarmon.wav')
+# transcribe_wav('./testDump/compressedHarmon.wav')
+
+
+# def transcribe_all(pathname):
+#     #Change working directory
+#     os.chdir(pathname)
+#     audio_files = os.listdir()
+
+#     print(f'We have {len(audio_files)} files to convert')
+#     for file in audio_files:
+#         #spliting the file into the name and the extension
+#         name, ext = os.path.splitext(file)
+        
+#         if ext == ".mp3":
+#             mp3_sound = AudioSegment.from_mp3(file)
+#             print(f'printing {name}')
+#             #rename them using the old name + ".wav"
+#             mp3_sound.export("{0}.wav".format(name), format="wav")
+
+
+def transcribe_all(split_directory):
+    for subdir, dirs, files in os.walk(split_directory):
+        for file in files:
+            print(os.path.join(subdir, file))
+
+transcribe_all(os.environ.get("split-audio-directory"))
+
+
+
+
+
+
+
 
 
 
